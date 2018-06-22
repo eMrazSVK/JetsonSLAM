a4267555e2f5feb68ff337a7fe7cea5b84b275b3
eMrazSVK <eduardmraz2@gmail.com>
2018-06-22 10:36:28 +0200

    Initial Commit

---
+++ pcl_testing.cpp
@@ -0,0 +1,118 @@
+#include <librealsense2/rs.hpp>
+#include <algorithm>            // std::min, std::max
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp> 
+#include <string> 
+
+typedef pcl::PointXYZRGB P_pcl;
+typedef pcl::PointCloud<P_pcl> point_cloud;
+typedef point_cloud::Ptr ptr_cloud;
+
+ 
+
+std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2::texture_coordinate texcoords)
+{
+
+   const int w = texture.get_width(), h = texture.get_height();
+   int x = std::min(std::max(int(texcoords.u*w + .5f), 0), w - 1);
+   int y = std::min(std::max(int(texcoords.v*h + .5f), 0), h - 1);
+   int idx = x * texture.get_bytes_per_pixel() + y * texture.get_stride_in_bytes();
+   const auto texture_data = reinterpret_cast<const uint8_t*>(texture.get_data());
+   return std::tuple<uint8_t, uint8_t, uint8_t>(
+
+  texture_data[idx], texture_data[idx + 1], texture_data[idx + 2]);
+}
+
+ 
+
+ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color){
+
+   ptr_cloud cloud(new point_cloud);
+
+   auto sp = points.get_profile().as<rs2::video_stream_profile>();
+   cloud->width = static_cast<uint32_t>(sp.width());
+   cloud->height = static_cast<uint32_t>(sp.height());
+   cloud->is_dense = false;
+   cloud->points.resize(points.size());
+
+   auto tex_coords = points.get_texture_coordinates();
+   auto vertices = points.get_vertices();
+
+   for (int i = 0; i < points.size(); ++i)
+
+  {
+
+  cloud->points[i].x = vertices[i].x;
+   cloud->points[i].y = vertices[i].y;
+   cloud->points[i].z = vertices[i].z;
+
+   std::tuple<uint8_t, uint8_t, uint8_t> current_color;
+   current_color = get_texcolor(color, tex_coords[i]);
+
+   cloud->points[i].r = std::get<0>(current_color);
+   cloud->points[i].g = std::get<1>(current_color);
+   cloud->points[i].b = std::get<2>(current_color);
+   }
+
+ 
+
+   return cloud;
+}
+int main() {
+
+    // Declare pointcloud object, for calculating pointclouds and texture mappings
+    rs2::pointcloud pc;
+
+    int i,j,count;
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+
+    // Create a configuration for configuring the pipeline with a non default profile
+    rs2::config cfg;
+
+    //Add desired streams to configuration
+    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
+    cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
+    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+
+    // Start streaming with default recommended configuration
+    pipe.start(cfg);
+    
+
+    // Camera warmup - dropping several first frames to let auto-exposure stabilize
+    rs2::frameset frames;
+    for(i = 0; i < 100; i++)
+    {
+        // Wait for all configured streams to produce a frame
+        frames = pipe.wait_for_frames();
+    }
+    
+    std::string save_filename;
+    count = 0;
+    
+    rs2::align align(RS2_STREAM_COLOR);
+    auto aligned_frames = align.process(frames);
+    
+
+    
+    frames = pipe.wait_for_frames();
+    auto depth = frames.get_depth_frame();
+    auto colored_frame = frames.get_color_frame();
+    auto points = pc.calculate(depth);
+    pc.map_to(colored_frame);
+    ptr_cloud cloud = points_to_pcl(points, colored_frame);
+    pcl::io::savePCDFileASCII("testujem.pcd", *cloud);
+  
+    return EXIT_SUCCESS;
+}
+
+
