16b0db7f296fc4ea7f2350c70abdba58759efe5b
eMrazSVK <eduardmraz2@gmail.com>
2018-06-22 14:19:08 +0200

    minor fixes

--- pcl_testing.cpp
+++ pcl_testing.cpp
@@ -10,28 +10,34 @@
 #include <pcl/visualization/cloud_viewer.h>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/thread/thread.hpp> 
-#include <string> 
+#include <boost/thread/thread.hpp>
+#include <string>
 
 typedef pcl::PointXYZRGB P_pcl;
 typedef pcl::PointCloud<P_pcl> point_cloud;
 typedef point_cloud::Ptr ptr_cloud;
 
+using namespace cv;
+
 
 std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2::texture_coordinate texcoords)
 {
     const int w = texture.get_width(), h = texture.get_height();
+    
     int x = std::min(std::max(int(texcoords.u*w + .5f), 0), w - 1);
     int y = std::min(std::max(int(texcoords.v*h + .5f), 0), h - 1);
+
     int idx = x * texture.get_bytes_per_pixel() + y * texture.get_stride_in_bytes();
     const auto texture_data = reinterpret_cast<const uint8_t*>(texture.get_data());
-    return std::tuple<uint8_t, uint8_t, uint8_t>(
-
-    texture_data[idx], texture_data[idx + 1], texture_data[idx + 2]);
+    return std::tuple<uint8_t, uint8_t, uint8_t>(texture_data[idx], texture_data[idx+1], texture_data[idx+2]);
 }
 
  
 ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color){
+
+    Mat colorr(Size(640, 480), CV_8UC3, (void*)color.get_data(), Mat::AUTO_STEP);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", colorr);
 
     ptr_cloud cloud(new point_cloud);
 
@@ -43,6 +49,7 @@ ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color
 
     auto tex_coords = points.get_texture_coordinates();
     auto vertices = points.get_vertices();
+
 
     for (int i = 0; i < points.size(); ++i)
     {
@@ -56,6 +63,7 @@ ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color
         cloud->points[i].r = std::get<0>(current_color);
         cloud->points[i].g = std::get<1>(current_color);
         cloud->points[i].b = std::get<2>(current_color);
+
     }
     
    return cloud;
@@ -77,26 +85,28 @@ int main() {
     cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
     cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
     cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
-
+   
     // Start streaming with default recommended configuration
     pipe.start(cfg);
 
     // Camera warmup - dropping several first frames to let auto-exposure stabilize
-    rs2::frameset frames;
     for(int i = 0; i < 100; i++)
     {
         // Wait for all configured streams to produce a frame
-        frames = pipe.wait_for_frames();
+        auto frames = pipe.wait_for_frames();
     }
-    
-    
-    frames = pipe.wait_for_frames();
+        
+    auto frames = pipe.wait_for_frames();
     auto depth = frames.get_depth_frame();
     auto colored_frame = frames.get_color_frame();
     auto points = pc.calculate(depth);
+    
     pc.map_to(colored_frame);
     ptr_cloud cloud = points_to_pcl(points, colored_frame);
     pcl::io::savePCDFileASCII("cloud_test.pcd", *cloud);
+
+    waitKey(0);
+
   
     return EXIT_SUCCESS;
 }
