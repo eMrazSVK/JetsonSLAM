fe7c4cde6dd3edeb9877906f50bf0fc51ea9f1e1
eMrazSVK <eduardmraz2@gmail.com>
2018-06-25 12:42:08 +0200

    Add comments

--- pcl_testing.cpp
+++ pcl_testing.cpp
@@ -20,10 +20,12 @@ typedef point_cloud::Ptr ptr_cloud;
 using namespace cv;
 
 
+// Get RGB values based on normals - texcoords, normals value [u v]
 std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2::texture_coordinate texcoords)
 {
     const int w = texture.get_width(), h = texture.get_height();
     
+    // convert normals [u v] to basic coords [x y]
     int x = std::min(std::max(int(texcoords.u*w + .5f), 0), w - 1);
     int y = std::min(std::max(int(texcoords.v*h + .5f), 0), h - 1);
 
@@ -32,16 +34,18 @@ std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2
     return std::tuple<uint8_t, uint8_t, uint8_t>(texture_data[idx], texture_data[idx+1], texture_data[idx+2]);
 }
 
- 
+
 ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color){
 
+    // OpenCV Mat for showing the rgb color image, just as part of processing
     Mat colorr(Size(640, 480), CV_8UC3, (void*)color.get_data(), Mat::AUTO_STEP);
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", colorr);
-
-    ptr_cloud cloud(new point_cloud);
-
+        
     auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    ptr_cloud cloud(new point_cloud);
+    
+    // Config of PCL Cloud object
     cloud->width = static_cast<uint32_t>(sp.width());
     cloud->height = static_cast<uint32_t>(sp.height());
     cloud->is_dense = false;
@@ -50,7 +54,8 @@ ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color
     auto tex_coords = points.get_texture_coordinates();
     auto vertices = points.get_vertices();
 
-
+    // Iterating through all points and setting XYZ coordinates
+    // and RGB values
     for (int i = 0; i < points.size(); ++i)
     {
         cloud->points[i].x = vertices[i].x;
@@ -60,6 +65,7 @@ ptr_cloud points_to_pcl(const rs2::points& points, const rs2::video_frame& color
         std::tuple<uint8_t, uint8_t, uint8_t> current_color;
         current_color = get_texcolor(color, tex_coords[i]);
 
+        // Reversed order- 2-1-0 because of BGR model used in camera
         cloud->points[i].r = std::get<2>(current_color);
         cloud->points[i].g = std::get<1>(current_color);
         cloud->points[i].b = std::get<0>(current_color);
@@ -100,9 +106,13 @@ int main() {
     auto depth = frames.get_depth_frame();
     auto colored_frame = frames.get_color_frame();
 
+    // Order here is crucial! 
+    // map_to() color frame has to be done befor point calculation
+    // otherwise texture won't be mapped
     pc.map_to(colored_frame);
     auto points = pc.calculate(depth);
 
+    // Actual calling of conversion and saving XYZRGB cloud to file
     ptr_cloud cloud = points_to_pcl(points, colored_frame);
     pcl::io::savePCDFileASCII("cloud_test.pcd", *cloud);
 
